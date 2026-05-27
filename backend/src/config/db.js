52334521298c269c7085037e26bc9bf49a799cab
eliezaylaa <eliezaylaa@gmail.com>
2026-05-27 05:27:03 +0200

    fix:rmeove process.exit and cleanup cnfigs

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -14,7 +14,6 @@ pool
   .then(() => console.log("Database connected"))
   .catch((err) => {
     console.error("Database connection error:", err);
-    process.exit(1);
   });
 
 module.exports = pool;
