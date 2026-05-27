3b52ea5b17df7eee4564b0ef991a27fb4390e771
eliezaylaa <eliezaylaa@gmail.com>
2026-05-27 05:54:51 +0200

    fix: support DATABASE_URL for production

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,20 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 5432,
+        database: process.env.DB_NAME || "pibly",
+        user: process.env.DB_USER || "postgres",
+        password: process.env.DB_PASSWORD || "postgres",
+      },
+);
 
 pool
   .connect()
